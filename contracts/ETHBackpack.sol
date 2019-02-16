c11643d14d4fb8c466e386ad0be2ba51a45f4be1
Sean.Mar <sean.martin.dev@gmail.com>
2019-02-16 13:55:46 -0700

    adding req to ETHBackpack.sol

--- contracts/ETHBackpack.sol
+++ contracts/ETHBackpack.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.5.0;
 import 'openzeppelin-solidity/contracts/token/ERC721/ERC721Full.sol';
 import 'openzeppelin-solidity/contracts/token/ERC721/ERC721Mintable.sol';
 
-contract MyNFT is ERC721Full, ERC721Mintable {
+contract MyNFT is ERC721Full, ERC721Mintable, ERC721Metadata.sol {
   constructor() ERC721Full("ETHBackpack", "BKP") public {
   }
 }
