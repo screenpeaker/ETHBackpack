67f3b17bec2f361f134abba9e1bb0d966bf405ca
ron <stonroner@gmail.com>
2019-02-16 15:01:00 -0700

    Changed from Mintable to MetadataMintable per Zeppelin

--- contracts/ETHBackpack.sol
+++ contracts/ETHBackpack.sol
@@ -1,9 +1,9 @@
 pragma solidity ^0.5.0;
 
 import 'openzeppelin-solidity/contracts/token/ERC721/ERC721Full.sol';
-import 'openzeppelin-solidity/contracts/token/ERC721/ERC721Mintable.sol';
+import 'openzeppelin-solidity/contracts/token/ERC721/ERC721MetadataMintable.sol';
 
-contract MyNFT is ERC721Full, ERC721Mintable, ERC721Metadata.sol {
+contract MyNFT is ERC721Full, ERC721MetadataMintable {
   constructor() ERC721Full("ETHBackpack", "BKP") public {
   }
 }
